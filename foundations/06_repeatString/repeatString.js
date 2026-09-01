ca80f1418c695727e1377f6aa2bc76ca4a5be3dc
Trieu Huynh <kk@Mac.lan>
2026-09-01 16:45:03 -0700

    foundations excercises 6 - 11

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times === 0) return "ERROR";
+    let string = "";
+    for(let i = 0; i < times; i++)
+    {
+        string += str;
+    }
+    return string
 };
 
 // Do not edit below this line
